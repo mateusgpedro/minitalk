059aafb80f00ba83350287be117a0a73684ea803
Mateus Guimaraes Pedro <maguimar@c1r9s4.42porto.com>
2023-01-12 18:56:01 +0000

    Bonus made

--- minitalk/src/client_bonus.c
+++ minitalk/src/client_bonus.c
@@ -6,7 +6,7 @@
 /*   By: maguimar <maguimar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 15:08:22 by maguimar          #+#    #+#             */
-/*   Updated: 2023/01/12 16:21:22 by maguimar         ###   ########.fr       */
+/*   Updated: 2023/01/12 18:50:08 by maguimar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,53 @@
 #include "../libft/libft.h"
 #include "../includes/ft_printf.h"
 
-int	main()
+void	send_msg_to_server(int server_pid, char message_to_send)
 {
+	int bits;
 
+	bits = 0;
+	while (bits <= 7)
+	{
+		if ((message_to_send >> bits) & 1)
+			kill(server_pid, SIGUSR1);
+		else
+			kill(server_pid, SIGUSR2);
+		usleep(150);
+		bits++;
+	}
+}
+
+void	client_handler(int sig, siginfo_t *info, void *ucontext)
+{
+	(void) sig;
+	(void) info;
+	(void) ucontext;
+	ft_printf("Message received!");
+}
+
+int	main(int argc, char **argv)
+{
+	struct sigaction	sa_client;
+	int	server_pid;
+	int	i;
+
+	if (argc != 3)
+	{
+		ft_printf("Please, verify the input: <./client> <PID> <message>");
+		return(0);
+	}
+	sa_client.sa_flags = SA_SIGINFO;
+	sa_client.sa_sigaction = &client_handler;
+	sigemptyset(&sa_client.sa_mask);
+	i = 0;
+	server_pid = ft_atoi(argv[1]);
+	if (sigaction(SIGUSR1, &sa_client, NULL) == -1)
+		ft_printf("Failed to receive signal");
+	while (argv[2][i])
+	{
+		send_msg_to_server(server_pid, argv[2][i]);
+		i++;
+	}
+	send_msg_to_server(server_pid, '\n');
+	send_msg_to_server(server_pid, '\0');
 }
--- minitalk/src/server_bonus.c
+++ minitalk/src/server_bonus.c
@@ -6,7 +6,7 @@
 /*   By: maguimar <maguimar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 15:08:19 by maguimar          #+#    #+#             */
-/*   Updated: 2023/01/12 17:17:21 by maguimar         ###   ########.fr       */
+/*   Updated: 2023/01/12 18:55:23 by maguimar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,48 @@
 #include "../libft/libft.h"
 #include "../includes/ft_printf.h"
 
-void	sighandler(int num)
+void	sighandler(int sig, siginfo_t *info, void *ucontext)
 {
+	static int bits;
+	static char final_msg;
 
+	(void) *ucontext;
+	if (sig == SIGUSR1)
+		final_msg = ((1 << bits) | final_msg);
+	else
+		final_msg = ((0 << bits) | final_msg);
+	bits++;
+	if (bits == 8)
+	{
+		write(1, &final_msg, 1);
+		if (final_msg == '\0')
+			kill(info->si_pid, SIGUSR1);
+		bits = 0;
+		final_msg = '\0';
+	}
 }
 
 int	main(int argc, char **argv)
 {
 	(void) argv;
+	struct sigaction	sa_server;
+	int	pid;
 
 	if(argc != 1)
 	{
 		ft_printf("Please, verify the input\n");
 		return (0);
 	}
-
-	struct sigaction	sa;
-	sa.sa_handler = &sighandler;
-
-	sigemptyset(&sa.sa_mask);
-
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
-
+	sa_server.sa_sigaction = &sighandler;
+	sa_server.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa_server.sa_mask);
+	pid = getpid();
+	ft_printf("PID: %d\n", pid);
+	sigaction(SIGUSR1, &sa_server, NULL);
+	sigaction(SIGUSR2, &sa_server, NULL);
+	if (sigaction(SIGUSR1, &sa_server, NULL) == -1
+		|| sigaction(SIGUSR2, &sa_server, NULL) == -1)
+		ft_printf("Failed to receive signal");
+	while (1)
+		pause();
 }
